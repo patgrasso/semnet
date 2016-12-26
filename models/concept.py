cb0830ecba9e42bce2cc7b51bbf6fc587bc63907
patgrasso <pgrasso@stevens.edu>
2016-12-26 13:50:48 -0500

    Replace Thing with Concept+parent reference
    Instead of having a Thing which references a parent Concept, just have
    other Concepts whose parent is a more broad Concept.

--- models/concept.py
+++ models/concept.py
@@ -3,40 +3,106 @@ concepts = []
 
 class Concept(object):
 
-    def __init__(self, name):
+    def __init__(self, name, parent=None, mods=None):
         self.name = name
-        self.attributes = {}
-        self.instances = []
+        self._attributes = {}
+        self.mods = mods or []
+        self.children = []
+        self.parent = parent
 
-        concepts.append(self)
+        if parent is None:
+            concepts.append(self)
 
     def __repr__(self, indent=0):
-        ret = ["%s <%s>" % (self.__class__.__name__, self.name)]
+        ret = ["%s <%s> %s" % (self.__class__.__name__, self.name, self.mods)]
         for attr_name, attr_values in self.attributes.items():
-            ret.append("  " + attr_name + ": ")
-            for attr_val in attr_values:
-                ret.append("    " + attr_val.__repr__(indent + 1))
-        return ("\n" + "    "*indent).join(ret)
+            ret.append("|-" + attr_name + ":")
+
+            for attr_val in attr_values[:-1]:
+                if attr_val != self:
+                    ret.append("|   " + attr_val.__repr__(indent + 1))
+
+            if len(attr_values) > 0 and attr_values[-1] != self:
+                ret.append("|   " + attr_values[-1].__repr__(indent + 1))
+
+        return ("\n" + "|   "*indent).join(ret)
 
     def __getitem__(self, name):
         return self.attributes[name]
 
     def __setitem__(self, name, value):
-        if name not in self.attributes:
-            self.attributes[name] = []
-        self.attributes[name].append(value)
-        return self
+        if name not in self._attributes:
+            self._attributes[name] = []
+        if value not in self._attributes[name]:
+            self._attributes[name].append(value)
+        return value
+
+    def _find_child(self, mods):
+        all_mods_in_self = all([mod in self.mods for mod in mods])
+        all_self_in_mods = all([mod in mods for mod in self.mods])
+
+        if all_mods_in_self and all_self_in_mods:
+            return self
+
+        for child in self.children:
+            found = child._find_child(mods)
+            if found is not None:
+                return found
+
+        if all_self_in_mods:
+            new_concept = Concept(self.name, self, mods)
+            self.children.append(new_concept)
+            return new_concept
+
+        return None
+
+    def _dot_label(self):
+        return '"' + self.name + '(' + ", ".join(self.mods) + ')' + '"'
+
+    def _to_dot(self, filename=None):
+        node_label = self._dot_label()
+
+        ret = []
+        if self.parent is not None:
+            ret.append(node_label + " -> " + self.parent._dot_label() +\
+                        " [style=dotted]")
+
+        for key, values in self._attributes.items():
+            for value in values:
+                ret.append(node_label + " -> " + value._dot_label() +\
+                        " [label=\"" + key + "\"]")
+
+        for child in self.children:
+            ret.append(child._to_dot())
+        return '\n'.join(ret)
+
+    @property
+    def attributes(self):
+        attrs = self.parent.attributes if self.parent is not None else {}
+        attrs.update(self._attributes)
+        return attrs
 
     @staticmethod
-    def get(name):
+    def get(name, mods=None):
+        mods = mods or []
+
         for concept in concepts:
             if concept.name == name:
-                return concept
-        new_concept = Concept(name)
-        concepts.append(new_concept)
-        return new_concept
+                return concept._find_child(mods)
+
+        return Concept(name)._find_child(mods)
 
     @staticmethod
     def concepts():
         return concepts
+
+    @staticmethod
+    def to_dot(filename):
+        f = open(filename, 'w')
+        f.write("digraph concept {\n")
+        for concept in concepts:
+            f.write(concept._to_dot())
+            f.write('\n')
+        f.write("\n}\n")
+        f.close()
 
